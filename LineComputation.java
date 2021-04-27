49097ee745950aeedc60cdc00baa697a73900969
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-27 16:15:00 +0530

    Adding: Equality

--- LineComputation.java
+++ LineComputation.java
@@ -4,10 +4,23 @@ public class LineComputation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		float x1=4 , y1=2;
-		float x2=2 , y2=5;
-		double len_of_line=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
-		int len=(int)Math.sqrt(len_of_line);
-		System.out.println(len);
+		int x1=2 , y1=3;
+		int x2=6 , y2=4;
+		int len1=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+		int len_of_line1=(int)Math.sqrt(len1);
+		System.out.println(len_of_line1);
+		
+		int p1=3 , q1=4;
+		int p2=7 , q2=1;
+		int len2=(p2-p1)*(p2-p1)+(q2-q1)*(q2-q1);
+		int len_of_line2=(int)Math.sqrt(len2);
+		System.out.println(len_of_line2);
+		
+		if(len_of_line1==len_of_line2) {
+			System.out.println("Two lines are equal");
+		}
+		else {
+			System.out.println("Two lines are not equal");
+		}
 	}
 }
